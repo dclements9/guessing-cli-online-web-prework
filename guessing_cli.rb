a3d425aaf2dea13cad712be85acd02d440d147be
Dylan Clements <dclements@protonmail.com>
2019-01-11 17:07:58 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
     while user_guess
       puts "Guess a number between 1 and 6."
       user_guess = gets.chomp
-      number = rand(1..6)
+      number = rand(1..6).to_s
     case user_guess
       when number
         puts "You guessed the correct number!"
