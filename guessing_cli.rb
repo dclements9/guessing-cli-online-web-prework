afebb6de2b78d818f5d474c2857fb0c54024688c
Dylan Clements <dclements@protonmail.com>
2019-01-11 17:07:21 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,12 +2,12 @@ def run_guessing_game
     user_guess = ""
     while user_guess
       puts "Guess a number between 1 and 6."
-      user_guess = gets.chomp.to_i
-      number = rand(1..6)
+      user_guess = gets.chomp
+      number = rand(1..6).to_s
     case user_guess
       when number
         puts "You guessed the correct number!"
-      when 'exit'
+      when "exit"
         puts "Goodbye!"
         break
       else
